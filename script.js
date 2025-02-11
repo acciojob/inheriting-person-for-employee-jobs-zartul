b8ee61f610e97ed5344734dc523aa959cb6a89f7
zartul <121471819+zartul@users.noreply.github.com>
2025-02-11 11:08:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Person(name, age) {
 	this.name=name;
 	this.age=age;
 	greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 	}
 }
 
