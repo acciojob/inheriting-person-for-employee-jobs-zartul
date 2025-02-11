ae993ab8f1d7a4d0614a11957936c02c338ca8c6
zartul <121471819+zartul@users.noreply.github.com>
2025-02-11 11:24:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // complete this js code
-function Person(name, age) {
-	this.name=name;
-	this.age=age;
+class Person() {
+	constructor (name, age){
+		this.name=name;
+		this.age=age;
+	}
 	greet(){
 		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 	}
 }
-
-function Employee(name, age, jobTitle) {
-	Person.call(this,name,age);
-	this.jobTitle=jobTitle;
+class Employee extends Person{
+	 constructor (name, age, jobTitle) {
+		 super(name,age);
+		this.jobTitle=jobTitle;
+	 }
 	jobGreet(){
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	}
