fd835981922a083d9791d207d612630c44602b00
zartul <121471819+zartul@users.noreply.github.com>
2025-02-11 10:57:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name=name;
+	this.age=age;
+	greet(){
+		console.log("Hello, my name is `${this.name}`, I am `${this.age}` years old.");
+	}
+}
 
-function Employee(name, age, jobTitle) {}
-
+function Employee(name, age, jobTitle) {
+	Person.call(this,name,age);
+	this.jobTitle=jobTitle;
+	jobGreet(){
+		console.log("Hello, my name is `${this.name}`, I am `${this.age}` years old, and my job title is `${this.jobTitle}`.")
+	}
+}
+Employee=Object.create(Person);
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
